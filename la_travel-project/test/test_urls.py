0781473375fa78bd0e611f77a0d5912b460da6a8
tuany9496 <55112873+tuany9496@users.noreply.github.com>
2019-11-05 21:30:39 -0800

    pytest

---
+++ la_travel-project/test/test_urls.py
@@ -0,0 +1,17 @@
+import pytest
+
+from django.urls import reverse, resolve
+
+class TestUrls:
+
+    def test_home_url(self):
+        path = reverse('home')
+        assert resolve(path).view_name == 'home'
+
+    # def test_signup_url(self):
+    #     path = reverse('map')
+    #     assert resolve(path).view_name == 'map'
+    #
+    # def test_login_url(self):
+    #     path = reverse('searched')
+    #     assert resolve(path).view_name == 'searched'
