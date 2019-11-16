17be9c0b11651da8b007e4362b0c1fe8891bac18
cjbaker707 <55448079+cjbaker707@users.noreply.github.com>
2019-11-15 17:42:46 -0800

    Add files via upload
    These two scripts create a calendar event in Outlook and only works in the windows environment since the script imports the  win32com.client API.  The scripts are coded differently (slight changes, one uses the appointment item and the other uses the appt item but both perform the same function.

---
+++ CreateCalendarEvent2.py
@@ -0,0 +1,14 @@
+import win32com.client
+def sendMeeting(): 
+ oOutlook = win32com.client.Dispatch("Outlook.Application")
+  appt = outlook.CreateItem(1) # AppointmentItem
+  appt.Start = "2018-10-28 10:10" # yyyy-MM-dd hh:mm
+  appt.Subject = "Subject of the meeting"
+  appt.Duration = 60 # In minutes (60 Minutes)
+  appt.Location = "Location Name"
+  appt.MeetingStatus = 1 # 1 - olMeeting; Changing the appointment to meeting. Only after changing the meeting status recipients can be added
+  
+  appt.Recipients.Add("test@test.com") # Don't end ; as delimiter
+
+  appt.Save()
+  appt.Send()
