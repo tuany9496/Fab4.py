f7d85dd4262a38221c0c1da35ab84c841fe9244b
cjbaker707 <55448079+cjbaker707@users.noreply.github.com>
2019-11-15 16:38:41 -0800

    Add files via upload
    Please install pip3 install pdfkit
    Also, please install wkhtmltopdf - https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf   This tool provides the functionality to convert an html page to a pdf

---
+++ Web2PDFa.py
@@ -0,0 +1,15 @@
+import requests
+import pdfkit
+
+url = input("Please enter the url of the file you want to download:")
+path = input("Please enter the file path ex. C:\Jim\Desktop: ")
+file_name = input("Please enter file name: ")
+if pdfkit.from_url(str(url), str(path + file_name)): # Check if method from_url returned True
+    print("Sucessfully created pdf from url")
+else:
+    print("Something went wrong")
+    print("Download starting.")
+#r = requests.get(pdf)
+
+#with open(path, 'wb') as f:
+ #   f.write(r.content)
